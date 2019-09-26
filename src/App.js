3428ae0de3e253ee36b4b49127832667ba7ad970
J Hill <chirolhi@usc.edu>
2019-09-25 20:16:03 -0700

    fix length of first timer

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default class App extends React.Component {
       loading: false,
       startTimer: false,
       startTimer2: true,
-      timer1Length: 3,
+      timer1Length: 30,
       timer2Length: 120,
       blockInput: false,
     };
